d7090c3b6362e63de5cccf81153fddfa6fdb7e0d
Jhorman Tito <jhormanrus@gmail.com>
2023-06-25 17:36:29 +0000

    use of native dialog as modal

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
-import { Dialog, DialogPanel, DialogTitle, DialogDescription } from '@headlessui/vue'
 import { store } from '../store'
 import packageJson from '../../package.json'
 
-const isOpen = ref(false)
+const modal = ref<HTMLDialogElement | null>(null)
+
+function openModal() {
+  modal.value!.showModal()
+}
+
+function closeModal() {
+  modal.value!.close()
+}
 
 onMounted(() => {
   if (store.isFirstTimeInVersion) {
@@ -13,58 +20,47 @@ onMounted(() => {
     openModal()
   }
 })
-
-function closeModal() {
-  isOpen.value = false
-}
-function openModal() {
-  isOpen.value = true
-}
 </script>
 
 <template>
   <button @click="openModal" title="Acerca de" type="button" class="btn w-7 h-7">
     <img class="dark:invert" src="/svg/about.svg" alt="letter i">
   </button>
-  <Dialog :open="isOpen" @close="closeModal" class="relative z-10">
-    <div class="fixed inset-0 bg-black bg-opacity-50" />
-    <div class="fixed inset-0 overflow-y-auto p-4">
-      <div class="flex min-h-full items-center justify-center">
-        <DialogPanel class="w-full max-w-[240px] overflow-hidden rounded-lg bg-light-background dark:bg-dark-background p-4 align-middle">
-          <DialogTitle as="h2" class="font-bold text-center leading-none">
-            Himnario Adventista Broadcast
-          </DialogTitle>
-          <DialogDescription class="text-sm mt-3">
-            <div class="flex flex-col gap-1">
-              <div>
-                <span class="font-bold">Versión</span>
-                <p>{{ packageJson.version }}</p>
-              </div>
-              <div>
-                <span class="font-bold">Notas de la versión</span>
-                <ul class="list-disc pl-6">
-                  <li>Se añadió un acceso rápido para activar <b>Autoconducir</b> desde el panel principal</li>
-                  <li>Se añadió el botón <b>Detener</b> para detener la reproducción (y cambiar de escena)</li>
-                </ul>
-              </div>
-              <div>
-                <span class="font-bold">Creador</span>
-                <p><a href="https://github.com/jhormanrus" target="_blank" rel="noopener noreferrer" class="link">Jhorman Rus</a></p>
-              </div>
-              <div>
-                <span class="font-bold">Repositorio</span>
-                <p><a href="https://github.com/jhormanrus/himnario-adventista-broadcast" target="_blank" rel="noopener noreferrer" class="link">himnario-adventista-broadcast</a></p>
-              </div>
-              <div>
-                <span class="font-bold">Licencia</span>
-                <p>MIT License</p>
-              </div>
-            </div>
-          </DialogDescription>
-        </DialogPanel>
+  <dialog ref="modal" class="backdrop:bg-black backdrop:bg-opacity-50 w-full max-w-[270px] rounded-lg bg-light-background dark:bg-dark-background text-black dark:text-white p-5 align-middle text-base">
+    <h1 class="font-bold text-center leading-none">
+      Himnario Adventista Broadcast
+    </h1>
+    <div class="flex flex-col gap-1 text-sm my-4">
+      <div>
+        <span class="font-bold">Versión</span>
+        <p>{{ packageJson.version }}</p>
+      </div>
+      <div>
+        <span class="font-bold">Notas de la versión</span>
+        <ul class="list-disc pl-6">
+          <li>Se añadió un acceso rápido para activar <b>Autoconducir</b> desde el panel principal</li>
+          <li>Se añadió el botón <b>Detener</b> para detener la reproducción (y cambiar de escena)</li>
+        </ul>
+      </div>
+      <div>
+        <span class="font-bold">Creador</span>
+        <p><a href="https://github.com/jhormanrus" target="_blank" rel="noopener noreferrer" class="link">Jhorman Rus</a></p>
+      </div>
+      <div>
+        <span class="font-bold">Repositorio</span>
+        <p><a href="https://github.com/jhormanrus/himnario-adventista-broadcast" target="_blank" rel="noopener noreferrer" class="link">himnario-adventista-broadcast</a></p>
+      </div>
+      <div>
+        <span class="font-bold">Licencia</span>
+        <p>MIT License</p>
       </div>
     </div>
-  </Dialog>
+    <div class="flex">
+      <button type="button" class="btn ml-auto" @click="closeModal">
+        Cerrar
+      </button>
+    </div>
+  </dialog>
 </template>
 
 <style scoped>
--- src/components/Settings.vue
+++ src/components/Settings.vue
@@ -1,12 +1,11 @@
 <script setup lang="ts">
 import { ref } from 'vue'
-import { Dialog, DialogPanel, DialogTitle, DialogDescription } from '@headlessui/vue'
 import { store, defaultValues } from '../store'
 
-const isOpen = ref(false)
+const modal = ref<HTMLDialogElement | null>(null)
 
 function openModal() {
-  isOpen.value = true
+  modal.value!.showModal()
 }
 
 function closeModal() {
@@ -20,7 +19,7 @@ function closeModal() {
   localStorage.setItem('obsWebsocketUrl', store.obsWebsocketUrl)
   localStorage.setItem('musicHostUrl', store.musicHostUrl)
   localStorage.setItem('hymnalApiUrl', store.hymnalApiUrl)
-  isOpen.value = false
+  modal.value!.close()
 }
 </script>
 
@@ -28,80 +27,73 @@ function closeModal() {
   <button @click="openModal" title="Configuración" type="button" class="btn w-7 h-7 ml-auto">
     <img class="dark:invert" src="/svg/gear.svg" alt="gear">
   </button>
-  <Dialog :open="isOpen" @close="closeModal" class="relative z-10">
-    <div class="fixed inset-0 bg-black bg-opacity-50"></div>
-    <div class="fixed inset-0 overflow-y-auto p-4">
-      <div class="flex min-h-full items-center justify-center">
-        <DialogPanel class="w-full max-w-[270px] overflow-hidden rounded-lg bg-light-background dark:bg-dark-background p-4 align-middle">
-          <DialogTitle as="h2" class="font-bold text-center leading-none">
-            Configuración
-          </DialogTitle>
-          <DialogDescription class="text-sm my-3">
-            <div class="mb-2">
-              <h3 class="font-bold">Al buscar:</h3>
-              <div class="flex items-center gap-1">
-                <label for="oi">Solo instrumental</label>
-                <input v-model="store.onlyInstrumental" type="checkbox" id="oi" class="ml-auto">
-              </div>
-              <div class="flex items-center gap-1">
-                <label for="am">Autorreproducir música</label>
-                <input v-model="store.autoplayMusic" type="checkbox" id="am" class="ml-auto">
-              </div>
-              <div class="flex items-center gap-1">
-                <label for="av">Autoconducir versos</label>
-                <input v-model="store.autodriveVerses" type="checkbox" id="av" class="ml-auto">
-              </div>
-              <!-- <div class="flex items-center gap-1">
-                <label for="sv">Split verses (for long verses)</label>
-                <input v-model="store.splitVerses" type="checkbox" id="sv" class="ml-auto">
-              </div> -->
-              <div class="flex items-center gap-1">
-                <label for="sch">Cambiar a escena de himno</label>
-                <input v-model="store.onSearchSwitchToHymnScene" type="checkbox" id="sch" class="ml-auto">
-              </div>
-              <div class="flex flex-col">
-                <label for="hs">Escena de himno</label>
-                <select v-model="store.onSearchHymnScene" class="input__text !px-1" id="hs">
-                  <option value="">ninguno</option>
-                  <option class="hidden" :value="store.onSearchHymnScene">{{ store.onSearchHymnScene }}</option>
-                  <option v-for="scene in store.sceneList" :value="scene.sceneName">{{ scene.sceneName }}</option>
-                </select>
-              </div>
-            </div>
-            <div class="mb-2">
-              <h3 class="font-bold">Al terminar la música:</h3>
-              <div class="flex flex-col">
-                <label for="sc">Cambiar a escena</label>
-                <select v-model="store.onMusicEndSwitchToScene" class="input__text !px-1" id="sc">
-                  <option value="">ninguno</option>
-                  <option class="hidden" :value="store.onMusicEndSwitchToScene">{{ store.onMusicEndSwitchToScene }}</option>
-                  <option v-for="scene in store.sceneList" :value="scene.sceneName">{{ scene.sceneName }}</option>
-                </select>
-              </div>  
-            </div>
-            <div class="mb-2">
-              <h3 class="font-bold">Red:</h3>
-              <div class="flex flex-col">
-                <label for="ow">Personalizar URL de OBS Websocket</label>
-                <input v-model="store.obsWebsocketUrl" type="text" class="input__text" id="ow" :placeholder="defaultValues.obsWebsocketUrl">
-              </div>  
-              <div class="flex flex-col">
-                <label for="mh">Personalizar URL de host de música</label>
-                <input v-model="store.musicHostUrl" type="text" class="input__text" id="mh" placeholder="default">
-              </div>  
-              <div class="flex flex-col">
-                <label for="ha">Personalizar URL de API de himnario</label>
-                <input v-model="store.hymnalApiUrl" type="text" class="input__text" id="ha" :placeholder="defaultValues.hymnalApiUrl">
-              </div>  
-            </div>
-          </DialogDescription>
-          <div class="mt-4 flex">
-            <button type="button" class="btn ml-auto" @click="closeModal">
-              Guardar y cerrar
-            </button>
-          </div>
-        </DialogPanel>
+  <dialog ref="modal" class="backdrop:bg-black backdrop:bg-opacity-50 w-full max-w-[270px] rounded-lg bg-light-background dark:bg-dark-background text-black dark:text-white p-5 align-middle text-base">
+    <h1 class="font-bold text-center leading-none">
+      Configuración
+    </h1>
+    <div class="text-sm my-4">
+      <div class="mb-2">
+        <h3 class="font-bold">Al buscar:</h3>
+        <div class="flex items-center gap-1">
+          <label for="oi">Solo instrumental</label>
+          <input v-model="store.onlyInstrumental" type="checkbox" id="oi" class="ml-auto">
+        </div>
+        <div class="flex items-center gap-1">
+          <label for="am">Autorreproducir música</label>
+          <input v-model="store.autoplayMusic" type="checkbox" id="am" class="ml-auto">
+        </div>
+        <div class="flex items-center gap-1">
+          <label for="av">Autoconducir versos</label>
+          <input v-model="store.autodriveVerses" type="checkbox" id="av" class="ml-auto">
+        </div>
+        <!-- <div class="flex items-center gap-1">
+          <label for="sv">Split verses (for long verses)</label>
+          <input v-model="store.splitVerses" type="checkbox" id="sv" class="ml-auto">
+        </div> -->
+        <div class="flex items-center gap-1">
+          <label for="sch">Cambiar a escena de himno</label>
+          <input v-model="store.onSearchSwitchToHymnScene" type="checkbox" id="sch" class="ml-auto">
+        </div>
+        <div class="flex flex-col">
+          <label for="hs">Escena de himno</label>
+          <select v-model="store.onSearchHymnScene" class="input__text !px-1" id="hs">
+            <option value="">ninguno</option>
+            <option class="hidden" :value="store.onSearchHymnScene">{{ store.onSearchHymnScene }}</option>
+            <option v-for="scene in store.sceneList" :value="scene.sceneName">{{ scene.sceneName }}</option>
+          </select>
+        </div>
+      </div>
+      <div class="mb-2">
+        <h3 class="font-bold">Al terminar la música:</h3>
+        <div class="flex flex-col">
+          <label for="sc">Cambiar a escena</label>
+          <select v-model="store.onMusicEndSwitchToScene" class="input__text !px-1" id="sc">
+            <option value="">ninguno</option>
+            <option class="hidden" :value="store.onMusicEndSwitchToScene">{{ store.onMusicEndSwitchToScene }}</option>
+            <option v-for="scene in store.sceneList" :value="scene.sceneName">{{ scene.sceneName }}</option>
+          </select>
+        </div>  
+      </div>
+      <div class="mb-2">
+        <h3 class="font-bold">Red:</h3>
+        <div class="flex flex-col">
+          <label for="ow">URL de OBS Websocket</label>
+          <input v-model="store.obsWebsocketUrl" type="text" class="input__text" id="ow" :placeholder="defaultValues.obsWebsocketUrl">
+        </div>  
+        <div class="flex flex-col">
+          <label for="mh">URL de host de música</label>
+          <input v-model="store.musicHostUrl" type="text" class="input__text" id="mh" placeholder="default">
+        </div>  
+        <div class="flex flex-col">
+          <label for="ha">URL de API de himnario</label>
+          <input v-model="store.hymnalApiUrl" type="text" class="input__text" id="ha" :placeholder="defaultValues.hymnalApiUrl">
+        </div>  
       </div>
     </div>
-  </Dialog>
+    <div class="flex">
+      <button type="button" class="btn ml-auto" @click="closeModal">
+        Guardar y cerrar
+      </button>
+    </div>
+  </dialog>
 </template>
