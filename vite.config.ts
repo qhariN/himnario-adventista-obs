05672914d629308399724be4a70bf43cc0954bd2
Jhorman <jhormanrus@gmail.com>
2023-08-14 02:47:49 +0000

    🚀 bump deps version

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+import { configDefaults } from 'vitest/config'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
@@ -12,5 +13,10 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  test: {
+    environment: 'happy-dom',
+    exclude: configDefaults.exclude,
+    root: fileURLToPath(new URL('./', import.meta.url))
   }
 })
