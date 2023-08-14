215568007f3732b4b698ab41bf14125305c17c9b
Jhorman <jhormanrus@gmail.com>
2023-08-14 02:55:55 +0000

    🧪 setup ci and test hymn search

---
+++ src/components/__tests__/HymnSearcher.spec.ts
@@ -0,0 +1,58 @@
+import { VueWrapper, flushPromises, mount } from '@vue/test-utils'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
+import HymnSearcherVue from '../HymnSearcher.vue'
+import sHymn from '@/services/HymnService'
+
+vi.spyOn(sHymn, 'all').mockResolvedValue([
+  {
+    id: 1,
+    number: 1,
+    title: "Cantad Alegres"
+  },
+  {
+    id: 2,
+    number: 2,
+    title: "Dá gloria al Señor"
+  }
+])
+
+describe("HymnSearcher", () => {
+  let wrapper: VueWrapper
+
+  beforeEach(async () => {
+    wrapper = mount(HymnSearcherVue)
+
+    expect(sHymn.all).toHaveBeenCalled()
+    await flushPromises()
+  })
+
+  it("has a list of hymns", async () => {
+    const hymns = wrapper.findAll('[data-test="hymn-item"]')
+
+    expect(hymns).toHaveLength(2)
+    expect(hymns[0].text()).toContain("Cantad Alegres")
+    expect(hymns[1].text()).toContain("Dá gloria al Señor")
+  })
+
+  describe("when searching", () => {
+    it("shows only the hymns that match the search", async () => {
+      const search = wrapper.get('[data-test="search-hymn"]')
+      await search.setValue("Cantad Alegres")
+
+      const hymns = wrapper.findAll('[data-test="hymn-item"]')
+
+      expect(hymns).toHaveLength(1)
+      expect(hymns[0].text()).toContain("Cantad Alegres")
+    })
+
+    it("is diacritics insensitive", async () => {
+      const search = wrapper.get('[data-test="search-hymn"]')
+      await search.setValue("da gloria al senor")
+
+      const hymns = wrapper.findAll('[data-test="hymn-item"]')
+
+      expect(hymns).toHaveLength(1)
+      expect(hymns[0].text()).toContain("Dá gloria al Señor")
+    })
+  })
+})
