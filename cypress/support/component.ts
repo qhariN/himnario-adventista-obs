6ef4211dac6288b31b5fa4d39a517be5307c8022
Jhorman <jhormanrus@gmail.com>
2024-03-08 21:29:08 +0000

    📦 bump deps and update vue project

--- cypress/support/component.ts
+++ cypress/support/component.ts
@@ -25,7 +25,6 @@ import { mount } from 'cypress/vue'
 // your custom command.
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
-/* eslint-disable @typescript-eslint/no-namespace */
 declare global {
   namespace Cypress {
     interface Chainable {
